11c2047307368d9e02f70290d11250e14220722e
Toscan0 <j.tiago.henriques@tecnico.ulisboa.pt>
2021-02-03 23:46:24 +0000

    img bingo done

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Image-Bingo
-Image Bingo
+
+
+
+Game made for scouts activities, but can be played with any image.\
+Just download the source code, change the images and rebuild the game.
+
+Made with Unity 2020.2.2f1
---
+++ ScoutBingo/Assets/Scripts/RandomImg.cs
@@ -0,0 +1,99 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+using Random = UnityEngine.Random;
+
+public class RandomImg : MonoBehaviour
+{
+    public Sprite[] bingoIntro;
+    public Sprite[] bingoNumbers;
+    public Sprite endGame;
+
+    private List<int> retrievedInds = new List<int>();
+
+    private int indice = 0;
+    private bool intro = true;
+    private bool firstTime = true; 
+
+    public Image image;
+    public GameObject video;
+
+    private void Start()
+    {
+        GetNextIntroImg();
+
+        video.GetComponent<VideoPlayer>().loopPointReached += CheckEnd; 
+    }
+
+
+    private void Update()
+    {
+        
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (intro)
+            {
+                GetNextIntroImg();
+            }
+            else
+            {
+                if (firstTime)
+                {
+                    image.enabled = false;
+                    video.SetActive(true);
+                }
+                else
+                {
+
+                    GetRandomBingoInt();
+                }
+            }
+        }
+    }
+
+    private void GetRandomBingoInt()
+    {
+        int r = Random.Range(0, bingoNumbers.Length);
+
+        if (!retrievedInds.Contains(r))
+        {
+            retrievedInds.Add(r);
+            image.sprite = bingoNumbers[r];
+        }
+        else
+        {
+            if(retrievedInds.Count == bingoNumbers.Length)
+            {
+                // game finished
+                image.sprite = endGame;
+            }
+            else
+            {
+                GetRandomBingoInt();
+            }
+        }
+    }
+
+    private void GetNextIntroImg()
+    {
+        image.sprite = bingoIntro[indice];
+        indice++;
+
+        if (indice > (bingoIntro.Length - 1))
+        {
+            intro = false;
+        }
+    }
+
+    void CheckEnd(UnityEngine.Video.VideoPlayer vp)
+    {
+        firstTime = false;
+
+        image.enabled = true;
+        video.SetActive(false);
+
+        GetRandomBingoInt();
+    }
+}
